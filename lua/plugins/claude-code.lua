b16d4246be691d3245f11daccc79ccd40675c94a
Thomas Smallwood <tbm.smallwood@gmail.com>
2026-02-26 18:39:02 +0000

    claude code config

---
+++ lua/plugins/claude-code.lua
@@ -0,0 +1,50 @@
+return {
+  "coder/claudecode.nvim",
+  lazy = false,
+  dependencies = { "folke/snacks.nvim" },
+  opts = {
+    log_level = "info",
+    focus_after_send = true,
+    terminal = {
+      git_repo_cwd = true,
+      snacks_win_opts = {
+        position = "float",
+        width = 0.85,
+        height = 0.85,
+        border = "rounded",
+        keys = {
+          hide = {
+            "<leader>jj",
+            function(self)
+              self:hide()
+            end,
+            mode = "t",
+            desc = "Hide Claude",
+          },
+        },
+      },
+    },
+  },
+  keys = {
+    { "<leader>j", nil, desc = "Claude Code" },
+    -- { "<C-/>", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" }, -- not working
+    { "<leader>jj", "<cmd>ClaudeCode<cr>", desc = "Toggle Claude" },
+    { "<leader>jb", "<cmd>ClaudeCodeAdd %<cr>", desc = "Add current buffer" },
+    { "<leader>js", "<cmd>ClaudeCodeSend<cr>", mode = "v", desc = "Send selection" },
+
+    -- I use snacks_picker_list, not supported yet. Claude Code currently only supports these 5.
+    -- {
+    --   "<leader>js",
+    --   "<cmd>ClaudeCodeTreeAdd<cr>",
+    --   desc = "Add file",
+    --   ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw" },
+    -- },
+    { "<leader>jr", "<cmd>ClaudeCode --resume<cr>", desc = "Resume session" },
+    { "<leader>jc", "<cmd>ClaudeCode --continue<cr>", desc = "Continue session" },
+    { "<leader>jm", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select model" },
+
+    -- Diff actions
+    { "<leader>ja", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+    { "<leader>jd", "<cmd>ClaudeCodeDiffDeny<cr>", desc = "Deny diff" },
+  },
+}
