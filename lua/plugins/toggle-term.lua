14ee499552c0254d48400116ea40401bf6fc0ee6
Thomas Smallwood <tbm.smallwood@gmail.com>
2026-03-10 17:43:02 +0100

    toggle term shell to handle both zsh and bash

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -10,7 +10,7 @@ return {
     open_mapping = [[<leader>tt]],
     direction = "float",
     float_opts = { border = "rounded" },
-    shell = vim.fn.exepath("zsh") .. " -l -i",
+    shell = (os.getenv("SHELL") or vim.fn.exepath("zsh") or vim.fn.exepath("bash")) .. " -l -i",
   },
 
   config = function(_, opts)
